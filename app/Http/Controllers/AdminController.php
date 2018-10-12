e90bcb4a5b55d023c31587517c46fe7280aee59d
TheOnlySmartBoy <o2jose43@gmail.com>
2018-10-12 19:43:04 +0300

    Added all auths

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
    public function login(){
-       return view(admin.admin_login);
+       return view('admin.admin_login');
    }
 }
