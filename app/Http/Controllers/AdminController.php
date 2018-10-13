b3866d935fcc40f70d455cb1f707a6315cfc6a51
TheOnlySmartBoy <o2jsmartboy@gmail.com>
2018-10-13 09:56:43 +0300

    Completed Admin login logic

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
-class AdminController extends Controller
-{
-   public function login(){
-       return view('admin.admin_login');
-   }
+class AdminController extends Controller {
+
+    public function login(Request $request) {
+        if ($request->isMethod('post')) {
+            $data = $request->input();
+            //Attempt to login
+            if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'role' => '1'])) {
+                echo "Login Successfull for Admin";
+                die;
+            } else if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'role' => '0'])) {
+                echo "Login Successfull for User";
+                die;
+            } else {
+                echo"Login Failed";
+                die;
+            }
+        }
+        return view('admin.admin_login');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin','AdminController@login');
+Route::match(['get','post'],'/admin','AdminController@login');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
