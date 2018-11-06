8dfcf70c139b22a3d9b429606109b84972464426
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-06 14:15:31 +0300

    House keeping

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -8,17 +8,17 @@
     </div>
     <div class="container-fluid"><hr>  
         @if(Session::has('flash_message_error'))
-            <div class="alert alert-danger alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>	
-                <strong>{!!session('flash_message_error')!!}</strong>
-            </div>
-            @endif
-            @if(Session::has('flash_message_success'))
-            <div class="alert alert-success alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>	
-                <strong>{!!session('flash_message_success')!!}</strong>
-            </div>
-            @endif
+        <div class="alert alert-danger alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{!!session('flash_message_error')!!}</strong>
+        </div>
+        @endif
+        @if(Session::has('flash_message_success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{!!session('flash_message_success')!!}</strong>
+        </div>
+        @endif
         <div class="row-fluid">
             <div class="span12">
                 <div class="widget-box">
@@ -27,7 +27,7 @@
                     </div>
                     <div class="widget-content nopadding">
                         <form class="form-horizontal" method="post" action="{{url('admin/update-pwd')}}" name="password_validate" id="password_validate" novalidate="novalidate">{{ csrf_field() }}
-                             <div class="control-group">
+                            <div class="control-group">
                                 <label class="control-label">Current Password</label>
                                 <div class="controls">
                                     <input type="password" name="current_pwd" id="current_pwd" />
