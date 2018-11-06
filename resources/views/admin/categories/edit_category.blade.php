c0bbe8cbcae0f58a4ee71411e0bf38891b1f8f49
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-06 10:39:04 +0300

    Edited the view with current data as the default form control data

--- resources/views/admin/categories/edit_category.blade.php
+++ resources/views/admin/categories/edit_category.blade.php
@@ -3,7 +3,7 @@
 <div id="content">
     <div id="content-header">
         <div id="breadcrumb"> <a href="{{url('admin/dashboard')}}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> 
-            <a href="{{url(admin/view_categories')}}">Categories</a> <a href="#" class="current">Edit Category</a> 
+            <a href="{{url('admin/view_categories')}}">Categories</a> <a href="#" class="current">Edit Category</a> 
         </div>
         <h1>Edit Category</h1>
     </div>
@@ -21,24 +21,24 @@
                         <h5>Add Category Form</h5>
                     </div>
                     <div class="widget-content nopadding">
-                        <form class="form-horizontal" method="post" action="{{url('admin/edit_category')}}" name="edit_category" id="edit_category" novalidate="novalidate">{{ csrf_field() }}
+                        <form class="form-horizontal" method="post" action="{{url('admin/edit_category/'.$categoriesDetails->id)}}" name="edit_category" id="edit_category" novalidate="novalidate">{{ csrf_field() }}
                             <div class="control-group">
                                 <label class="control-label">Category Name</label>
                                 <div class="controls">
-                                    <input type="text" name="category_name" id="category_name">
+                                    <input type="text" name="category_name" id="category_name" value="{{ $categoriesDetails->name }}">
                                 </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label">Description</label>
                                 <div class="controls">
-                                    <textarea name="description" id="description"></textarea>
+                                    <textarea name="description" id="description">{{ $categoriesDetails->description }}</textarea>
                                 </div>
                             </div> 
 
                             <div class="control-group">
                                 <label class="control-label">URL</label>
                                 <div class="controls">
-                                    <input type="text" name="url" id="url">
+                                    <input type="text" name="url" id="url" value="{{ $categoriesDetails->url }}">
                                 </div>
                             </div>
                             <div class="form-actions">
