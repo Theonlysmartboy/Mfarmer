0b15aaa3d5c5e8da744d8d8f8c12e463ee8decb5
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-07 19:30:08 +0300

    Added an add products view file

---
+++ resources/views/admin/products/add_product.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.adminLayout.admin_design')
+@section('content')
+<div id="content">
+    <div id="content-header">
+        <div id="breadcrumb"> <a href="{{url('admin/dashboard')}}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> 
+            <a href="#">Products</a> <a href="#" class="current">Add Products</a> 
+        </div>
+        <h1>Add Products</h1>
+    </div>
+    <div class="container-fluid"><hr>
+        @if(Session::has('flash_message_error'))
+        <div class="alert alert-danger alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{!!session('flash_message_error')!!}</strong>
+        </div>
+        @endif
+        @if(Session::has('flash_message_success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{!!session('flash_message_success')!!}</strong>
+        </div>
+        @endif
+        
+        
+    </div>
+</div>
+@endsection
