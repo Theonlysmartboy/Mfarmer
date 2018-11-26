0ae85b381c6122764bc4059e86f6bc840b59fdd3
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-26 13:58:02 +0300

    Housekeeping

--- resources/views/layouts/adminlayout/admin_design.blade.php
+++ resources/views/layouts/adminlayout/admin_design.blade.php
@@ -55,30 +55,28 @@
         <script src="{{asset('js/backend_js/matrix.interface.js')}}"></script> 
         <script src="{{asset('js/backend_js/jquery.gritter.min.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-        <!--
-        <script src="{{asset('js/backend_js/matrix.dashboard.js')}}"></script-->
         <script type="text/javascript">
 // This function is called from the pop-up menus to transfer to
 // a different page. Ignore if the value returned is a null string:
 function goPage(newURL) {
 
 // if url is empty, skip the menu dividers and reset the menu selection to default
-    if (newURL != "") {
+if (newURL != "") {
 
-        // if url is "-", it is this page -- reset the menu:
-        if (newURL == "-") {
-            resetMenu();
-        }
-        // else, send page to designated URL            
-        else {
-            document.location.href = newURL;
-        }
-    }
+// if url is "-", it is this page -- reset the menu:
+if (newURL == "-") {
+    resetMenu();
+}
+// else, send page to designated URL            
+else {
+    document.location.href = newURL;
+}
+}
 }
 
 // resets the menu selection upon entry to this page:
 function resetMenu() {
-    document.gomenu.selector.selectedIndex = 2;
+document.gomenu.selector.selectedIndex = 2;
 }
         </script>
     </body>
