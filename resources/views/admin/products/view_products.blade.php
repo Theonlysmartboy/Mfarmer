d509dead98d49523e64ac7b84a9b7defbe42164e
TheOnlySmartBoy <o2jose43@gmail.com>
2018-11-26 14:08:33 +0300

    Edited Url for editing products

--- resources/views/admin/products/view_products.blade.php
+++ resources/views/admin/products/view_products.blade.php
@@ -31,11 +31,10 @@
                             <thead>
                                 <tr>
                                     <th>ID</th>
-                                    <th>CATEGORY ID</th>
+                                    <th>CATEGORY</th>
                                     <th>NAME</th>
                                     <th>PRODUCT CODE</th>
                                     <th>COLOR</th>
-                                    <th>DESCRIPTION</th>
                                     <th>PRICE</th>
                                     <th>IMAGE</th>
                                     <th>ACTION</th>
@@ -49,25 +48,38 @@
                                     <td>{{ $product->product_name }}</td>
                                     <td>{{ $product->product_code }}</td>
                                     <td>{{ $product->product_color }}</td>
-                                    <td class="text-center">{{ $product->description }}</td>
-                                    <td class="text-center">{{ $product->price }}</td>
+                                    <td class="text-center">Ksh &nbsp;{{ $product->price }}</td>
                                     <td>
-                                    @if(!empty($product->image))
-                                    <img src="{{asset('images/frontend_images/products/small/'. $product->image )}}" style="width:50px;"/>
-                                   @endif
-                                   </td>
-                                    <td><a href="#" class="btn btn-success btn-mini">View <i class="icon icon-eye-open"></i></a> | 
-                                        <a href="{{url('admin/edit_category/'.$product->id)}}" class="btn btn-warning btn-mini">Edit <i class="icon icon-edit"></i></a> | 
+                                        @if(!empty($product->image))
+                                        <img src="{{asset('images/frontend_images/products/small/'. $product->image )}}" style="width:50px;"/>
+                                        @endif
+                                    </td>
+                                    <td><a href="#productModal{{ $product->id }}" data-toggle="modal" class="btn btn-success btn-mini">View <i class="icon icon-eye-open"></i></a> | 
+                                        <a href="{{url('admin/edit_product/'.$product->id)}}" class="btn btn-warning btn-mini">Edit <i class="icon icon-edit"></i></a> | 
                                         <a rel="{{$product->id}}" rel1="delete_category" href="javascript:" class="btn btn-danger btn-mini deleteCategory">Delete <i class="icon icon-trash"></i></a></td>
                                 </tr>
-                                @endforeach
+                            <div id="productModal{{ $product->id }}" class="modal hide">
+                                <div class="modal-header bg-blue-dark">
+                                    <button data-dismiss="modal" class="close" type="button">×</button>
+                                    <h3 class="text-center">{{ $product->category_name }} {{ $product->product_name }}</h3>
+                                </div>
+                                <div class="modal-body">
+                                    @if(!empty($product->image))
+                                    <p class="text-center">
+                                    <img src="{{asset('images/frontend_images/products/medium/'. $product->image )}}" class="img-responsive text-center"/>
+                                    </p>
+                                    @endif
+                                    <p class="text-center bg-primary">DESCRIPTION: {{$product->description}}</p>
+                                    <p class="text-center">PRICE: Ksh &nbsp;{{$product->price}}</p>
+                                </div>
+                            </div>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 @endsection
