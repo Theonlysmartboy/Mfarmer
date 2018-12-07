d0bc5c484b9b64799100205f2789d1ef69d5f761
TheOnlySmartBoy <o2jose43@gmail.com>
2018-12-07 08:58:57 +0300

    Edited Url for editing products

--- resources/views/admin/products/edit_product.blade.php
+++ resources/views/admin/products/edit_product.blade.php
@@ -20,8 +20,67 @@
             <strong>{!!session('flash_message_success')!!}</strong>
         </div>
         @endif
-        
-        
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="widget-box">
+                    <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
+                        <h5>Edit Product Form</h5>
+                    </div>
+                    <div class="widget-content nopadding">
+                        <form enctype="multipart/form-data" class="form-horizontal" method="post" action="{{url('admin/edit_product/'.$productDetails->id)}}" name="edit_product" id="edit_product" novalidate="novalidate">{{ csrf_field() }}
+                            <div class="control-group">
+                                <label class="control-label">Category </label>
+                                <div class="controls">
+                                    <select name="category_id" style="width: 220px;">
+                                        <?php echo $categories_dropdown ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Name</label>
+                                <div class="controls">
+                                    <input type="text" name="product_name" id="product_name" value="{{$productDetails->product_name}}">
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Code</label>
+                                <div class="controls">
+                                    <input type="text" name="product_code" id="product_code" value="{{$productDetails->product_code}}">
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Color</label>
+                                <div class="controls">
+                                    <input type="text" name="product_color" id="product_color" value="{{$productDetails->product_color}}">
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Description</label>
+                                <div class="controls">
+                                    <textarea name="product_desc" id="product_desc" value="">{{$productDetails->description}}</textarea>
+                                </div>
+                            </div> 
+                            <div class="control-group">
+                                <label class="control-label">Cost</label>
+                                <div class="controls">
+                                    <input type="text" name="product_cost" id="product_cost"value="{{$productDetails->price}}">
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <label class="control-label">Image</label>
+                                <div class="controls">
+                                    <input type="file" name="product_image" id="product_image" value="{{$productDetails->image}}>
+                                </div>
+                            </div>
+                            <div class="form-actions">
+                                <input type="submit" value="Save" class="btn btn-success">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
